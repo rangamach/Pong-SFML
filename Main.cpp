90465414bff7326380f072f7e234d653f0cb12c7
rangamach <117797904+rangamach@users.noreply.github.com>
2024-12-24 13:25:36 +0530

    Added poll events

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Header/Core/GameWindowManager.h"
+#include "Header/Event/EventManager.h"
 
 using namespace Core;
+using namespace Eventmanager;
 
 int main()
 {
 	GameWindowManager game_window_manager;
+	EventManager event_manager;
 	game_window_manager.initialize();
 	while (game_window_manager.isGameRunning())
 	{
+		event_manager.pollEvents(game_window_manager.getGameWindow());
 		game_window_manager.render();
 	}
 }
